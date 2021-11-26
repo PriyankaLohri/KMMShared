2740c08d440eff6b6311853e7603df1d0c4c1868
Priyanka <priyanka@Priyankas-MacBook-P.local>
2021-11-26 08:58:39 +0530

    added KMM code

---
+++ MyKMMApplication/sharedKMM/src/commonMain/kotlin/com/example/mykmmapplication/Greeting.kt
@@ -0,0 +1,11 @@
+package com.example.mykmmapplication
+
+class Greeting {
+    fun greeting(): String {
+        var number = 5.5
+        print("Result = ${(number)}")
+        return "Hello Priyanka K! \n Welcome to, ${Platform().platform}!"
+
+    }
+
+}
